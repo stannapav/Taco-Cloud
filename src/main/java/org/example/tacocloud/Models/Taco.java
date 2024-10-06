b912259b7322817de8bbcb5969af290f5fd430ef
Anna Striharchuk <stannapav@gmail.com>
2024-10-06 20:21:43 +0300

    adding taco< taco order and ingridients

---
+++ src/main/java/org/example/tacocloud/Models/Taco.java
@@ -0,0 +1,10 @@
+package org.example.tacocloud.Models;
+
+import lombok.Data;
+import java.util.List;
+
+@Data
+public class Taco {
+    private String name;
+    private List<Ingredient> ingredients;
+}
